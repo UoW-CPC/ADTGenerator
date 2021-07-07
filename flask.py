50998859849cf813fea69f65f6a377b670b88982
hmdcpc2021 <78861297+hmdcpc2021@users.noreply.github.com>
2021-07-07 08:51:02 +0100

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+import json, validators, urllib.request, os.path, excel2json
+
+app = Flask(__name__)
+@app.route('/mdt', methods = ["GET", "POST"])
+
+def mdt():
+    file_path = 'microservice.xlsx'
+    if os.path.exists(file_path):
+        if file_path.endswith('.txt'):
+            with open(file_path,  encoding='utf-8', errors='ignore') as j:        
+
+                lines = j.readlines()
+                lines = [line.replace('\t', '') for line in lines]
+            
+                for url in lines: 
+                    if validators.url(url):
+                        response = urllib.request.urlopen(url)
+                        data = json.loads(response.read())
+                        return data
+                    else:
+                        return json.dumps(lines, indent= True)
+        
+        else:
+            excel2json.convert_from_file(file_path)
+            xls = pd.ExcelFile(file_path)
+            df1 = pd.read_excel(xls)
+            return df1.to_json()
+            
+                    
+    else:
+        return jsonify({"message":"File not found: Please use a valid file"})
+
+@app.route('/algodt', methods = ["GET", "POST"])
+
+def algodt():
+    file_path = 'algorithm.xlsx'
+    if os.path.exists(file_path):
+        if file_path.endswith('.txt'):
+            with open(file_path,  encoding='utf-8', errors='ignore') as j:        
+
+                lines = j.readlines()
+                lines = [line.replace('\t', '') for line in lines]
+            
+                for url in lines: 
+                    if validators.url(url):
+                        response = urllib.request.urlopen(url)
+                        data = json.loads(response.read())
+                        return data
+                    else:
+                        return json.dumps(lines, indent= True)
+        
+        else:
+            excel2json.convert_from_file(file_path)
+            xls = pd.ExcelFile(file_path)
+            df1 = pd.read_excel(xls)
+            return df1.to_json()
+            
+                    
+    else:
+        return jsonify({"message":"File not found: Please use a valid file"})
+
+@app.route('/idt', methods = ["GET", "POST"])
+
+def idt():
+    file_path = 'infrastructure.xlsx'
+    if os.path.exists(file_path):
+        if file_path.endswith('.txt'):
+            with open(file_path,  encoding='utf-8', errors='ignore') as j:        
+
+                lines = j.readlines()
+                lines = [line.replace('\t', '') for line in lines]
+            
+                for url in lines: 
+                    if validators.url(url):
+                        response = urllib.request.urlopen(url)
+                        data = json.loads(response.read())
+                        return data
+                    else:
+                        return json.dumps(lines, indent= True)
+        
+        else:
+            excel2json.convert_from_file(file_path)
+            xls = pd.ExcelFile(file_path)
+            df1 = pd.read_excel(xls)
+            return df1.to_json()
+            
+                    
+    else:
+        return jsonify({"message":"File not found: Please use a valid file"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
