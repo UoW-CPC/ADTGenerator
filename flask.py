625de7886044c5404b30194427619d0f601f17d9
Hamed Hamzeh <78861297+hmdcpc2021@users.noreply.github.com>
2021-08-11 11:58:59 +0100

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,19 +1,31 @@
-from flask import Flask, jsonify, request
-import yaml, logging, logging.config, json
+from flask import Flask
+import yaml, logging, logging.config, json, os
 from logging.config import dictConfig
 
 app = Flask(__name__)
 
 try: 
-    with open('config_test.yaml','r') as stream:
-        config = yaml.safe_load(stream)
+    with open('config_test.yaml', 'r') as f:
+        config = yaml.safe_load(f)
         app.config.from_object(config)
-        logging.config.dictConfig(config)
-        app.logger.info('Reading the Config File')
+        app.logger.info('Loading is successful')
 except Exception as e:
     app.logger.error('Failed to load config file: '+ str(e))
 
-#logging.config.dictConfig(config)
+logging.basicConfig(filename='record.log', level=logging.DEBUG, 
+    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+fileHandler = logging.FileHandler("summary.log")
+fileHandler.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+app.logger.addHandler(fileHandler)
+app.logger.addHandler(streamHandler)
+app.logger.info("Logging is set up.")
+
+
+logging_configuration = app.config.get('LOGGING')
+if logging_configuration:
+    logging.config.dictConfig(logging_configuration)
 
 @app.route('/mdt', methods = ["GET", "POST"])
 
@@ -26,12 +38,12 @@ def compile_mdt():
             
         except ValidationError as err:
             app.logger.error("No valid json is found!")
-    # invoking library here ... #
+    # invoking library here ...
     # .........
     # Then invoking the libarary
     app.logger.info('The library has been invoked!')
     mdt_test = "This is mdt"
-    return mdt_test   
+    return 'This is MDT! {}'.format(app.config.get('LOGGING'))   
 
 @app.route('/algodt', methods = ["GET", "POST"])
 
@@ -48,7 +60,7 @@ def compile_algodt():
     # Then invoking the libarary
     app.logger.info('The library has been invoked!')
     mdt_test = "This is mdt"
-    return mdt_test  
+    return 'This is ADT! {}'.format(app.config.get('LOGGING'))  
 
 @app.route('/idt', methods = ["GET", "POST"])
 
@@ -65,23 +77,14 @@ def compile_idt():
     # Then invoking the libarary
     app.logger.info('The library has been invoked!')
     mdt_test = "This is mdt"
-    return mdt_test  
+    return 'This is IDT! {}'.format(app.config.get('LOGGING'))  
 
 # composing code comes here once the results are gotten from compile libraries and functions
 # def compose_dt(mdt_test, algodt_test, idt_test):
 #  ....  getting libraries and put the results in CSAR
-    
-if __name__ == "__main__":
-    logFormatStr = '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
-    logging.basicConfig(format = logFormatStr, filename = "global.log", level=logging.DEBUG)
-    formatter = logging.Formatter(logFormatStr,'%m-%d %H:%M:%S')
-    fileHandler = logging.FileHandler("summary.log")
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(formatter)
-    streamHandler = logging.StreamHandler()
-    streamHandler.setLevel(logging.DEBUG)
-    streamHandler.setFormatter(formatter)
-    app.logger.addHandler(fileHandler)
-    app.logger.addHandler(streamHandler)
-    app.logger.info("Logging is set up.")
-    app.run(debug = True)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', debug=True)
+
+
