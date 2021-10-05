a23788a2a789dfeaab1a2149327ddc0114d6f49a
Hamed Hamzeh <78861297+hmdcpc2021@users.noreply.github.com>
2021-10-05 17:32:29 +0100

    Update adt-gen.py

--- restapi/adt-gen.py
+++ restapi/adt-gen.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, request
-import yaml, logging, logging.config, json
+import yaml, logging, logging.config, json, argparse
 from logging.config import dictConfig
 from flask_restful import reqparse
 #from flask_oidc import OpenIDConnect
 from app_config import LOG_FORMAT, CONF
 
 app = Flask(__name__)
+
+log = logging.getLogger('adtg_compiler')
+log.info("This is ADT generator compiler Configuration")
 #oidc = OpenIDConnect(app)
 try:
     app.config.from_object(__name__)
@@ -15,26 +18,24 @@ try:
 except Exception as e:
     app.logger.error('Failed to update config dictionary: ' + str(e))
 
+parser = argparse.ArgumentParser(description='processing arguments.')
+parser.add_argument('--aaa', dest = 'config_variable', default = './config_test.yaml' ,
+                    help='Setting arguments')
 
 @app.route('/v1/adtg/compile/mdt', methods = ["GET", "POST"])
 #@oidc.accept_token(require_token=True)
 def compile_mdt():
-    # parser = reqparse.Requestparser()
-    # parser.add_argument('id', type=str, required=True)
-    # id = args['id']
     # Token verification
     # ...
     # invoking library here ...
     # .........
+    print('This is adt')
     app.logger.info('The library has been invoked!')
     return jsonify({'success':'This is MDT'}), 200   
-
+    #return 'This is MDT! {}'.format(app.config.get('logging'))  
 @app.route('/v1/adtg/compile/algodt', methods = ["GET", "POST"])
 #@oidc.accept_token(require_token=True)
 def compile_algodt(id):
-    # parser = reqparse.Requestparser()
-    # parser.add_argument('id', type=str, required=True)
-    # id = args['id']
     # Token verification
     # ...
     # invoking library here ... #
@@ -45,9 +46,6 @@ def compile_algodt(id):
 @app.route('/v1/adtg/compile/idt', methods = ["GET", "POST"])
 #@oidc.accept_token(require_token=True)
 def compile_idt(id):
-    # parser = reqparse.Requestparser()
-    # parser.add_argument('id', type=str, required=True)
-    # id = args['id']
     # Token versification
     # ...
     # invoking library here ... #
@@ -59,19 +57,8 @@ def compile_idt(id):
 # def compose_dt(mdt_test, algodt_test, idt_test):
 #  ....  getting libraries and put the results in CSAR
 
-
 if __name__ == '__main__':
-    logging.basicConfig(format = LOG_FORMAT, filename = "record.log", level=logging.DEBUG)
-    formatter = logging.Formatter(LOG_FORMAT,'%m-%d %H:%M:%S')
-    fileHandler = logging.FileHandler("summary.log")
-    fileHandler.setLevel(logging.DEBUG)
-    fileHandler.setFormatter(formatter)
-    streamHandler = logging.StreamHandler()
-    streamHandler.setLevel(logging.DEBUG)
-    streamHandler.setFormatter(formatter)
-    app.logger.addHandler(fileHandler)
-    app.logger.addHandler(streamHandler)
-    app.logger.info("Logging is set up.")
+    logging.config.dictConfig(CONF['logging'])
     app.run(debug=True, port=5001)
 
 
