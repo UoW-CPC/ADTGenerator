10470dfc96ad3874696d1b4a32e260112fd08819
Hamed Hamzeh <78861297+hmdcpc2021@users.noreply.github.com>
2021-10-06 08:27:41 +0100

    Update app_config.py

--- restapi/app_config.py
+++ restapi/app_config.py
@@ -2,4 +2,4 @@ import yaml
 with open('config_test.yaml', 'r') as f:
     CONF = yaml.safe_load(f)
 
-LOG_FORMAT = '%(asctime)s %(levelname)s: %(filename)s %(funcName)s:%(lineno)d : %(message)s'
+#LOG_FORMAT = '%(asctime)s %(levelname)s: %(filename)s %(funcName)s:%(lineno)d : %(message)s'
