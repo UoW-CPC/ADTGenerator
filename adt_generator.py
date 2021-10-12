559da23b54f7a05d24d5c8a14a57afc29819fd91
Dimitris Kagialis <kagialisd@gmail.com>
2021-10-12 09:36:17 +0300

    refactor adt_generator.py

--- adt_generator.py
+++ adt_generator.py
@@ -4,17 +4,22 @@
 # Created By  : Dimitris Kagialis
 # Created Date: 07/10/2021
 # version ='1.0'
+# Contributors:
+# Updated at  : 12/10/2021
+# Tested at   : 12/10/2021
 # ---------------------------------------------------------------------------
 """ ADT-generator main
     Usage:
-    Initiate program packages with function init.
-    List of modules:
+    Initiate program packages with a function init.
+    List of components:
         RestAPI
         Compiler
         Logger   - always enabled
         Debugger - disabled by default
     Implementation:
-    Functional programming
+        Functional programming
+    Tests:
+
  """
 
 # Packages wide imports - step 1
@@ -22,43 +27,53 @@ import sys # to get root path
 from init import init # to initiate ADT-generator
 
 # Initiate ADT-generator modules - step 2
+# Question: how to pass the path? eg. ENV variable
+
 init(sys.path[0])
 
 # Import logger and log first record - step 3
 from utils.logger import logger
 logger.info('ADT-generator has been started')
 
-from compiler import compiler
-from tests import test_compiler_dicts
-metadata = test_compiler_dicts.mdt
-test_run = compiler.compile("MDT.yaml",metadata)
-print(test_run)
-# Import debugger required packages to be able to use the debugger decorator on functions - step 4
+
+# Import debugger to decorator module functions - step 4
 from utils import debugger
-import inspect
-print(__package__)
-# sample function to present debugger usage
-@debugger.debug
-def test():
-    '''
-    testttttt
-    :return:
-    '''
-    # call functions from a get_scope package
-    from tests.test_adt_generator import add, subadd
-    logger.info('Call package functions from a get_scope function')
-    ttt = 1
-    ttt = 2
-    logger.info(globals())
-    logger.info(locals())
-    add(1,1)
-    subadd(1,1)
-    add(1, 1)
+
 
 if __name__ == "__main__":
     # Main starts - step 4
     logger.info('Main has been started')
-    #Testing debugger and logger
+
+    #TESTS
+
+    # Compiler
+    from compiler import compiler
+    from tests import test_compiler_dicts
+    metadata = test_compiler_dicts.mdt
+    test_run = compiler.compile("MDT.yaml", metadata)
+    print(test_run)
+
+    #  Debugging
+    # sample function to present debugger usage
+    @debugger.debug
+    def test():
+        '''
+        test docstring
+        :return:
+        '''
+        # call functions from a get_scope package
+        from tests.test_adt_generator import add, subadd
+        logger.info('Call package functions from a get_scope function')
+        ttt = 1
+        ttt = 2
+        logger.info(globals())
+        logger.info(locals())
+        add(1, 1)
+        subadd(1, 1)
+        add(1, 1)
+        return 'test debugging from test'
+
+    #Test debugger and logger
     test()
     from tests.test_adt_generator import add,subsubadd
     logger.info('Call package functions from main')
