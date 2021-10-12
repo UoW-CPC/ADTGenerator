40721b4e59c4ec1faa188788c119151300a29abb
Dimitris Kagialis <kagialisd@gmail.com>
2021-10-12 14:37:17 +0300

    capture exceptions add logger add debugger - decorate update docstring

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,73 +1,130 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # Created By  : Dimitris Kagialis
 # Created Date: 05/10/2021
 # version ='1.0'
+# Contributors:
+# Updated at  : 12/10/2021
+# Tested at   : 12/10/2021
 # ---------------------------------------------------------------------------
 """ Compiler module
-    Logs program flow and nested calls.
-    Implementation:
-        Use of the built-in logging package.
-        Debugger can be initialized only once.
-        Use of a decorator to wrap callee function and collect required information.
-    Configuration parameters:
-        path: path to debugging logs folder [Default value - project root path]
-        folder: debugging logs folder name [Default value 'debug']
-        level: debugging level - allowed values 'None', 'high', 'low', 'full' [default value 'None']
-        - None, the debugger is disabled.
-        - high, collects the caller module, function and line, and the callee module, function and line.
-        - low, collects high + input arguments and return.
-        - full, collects low + callee function globals.
-        debug_scope: dictionary with packages, modules, functions to include in the debugging phase [default value -  empty dict, debugging enabled for the whole project]
-    Functions:
-        init - initialize the debugger.
-        debug_func - debugger decorator, wraps the callee function and collects required information.
-        debug - callable function of the decorator, enables the debugger to pass arguments in the decorator.
+    Creates DTs (Description Templates) by using Jinja2.
     Usage:
-        To initialize the debugger:
-            from utils import debugger
-            debugger.init(path, folder, level, debug_scope)
-        To use the debugger:
-            from utils.debugger import debugger
-            @debugger.debug
-            def sammple_func():
-                pass
+        To initiate the compile:
+            from compiler import compiler
+            compiler.init(templates_path,modules)
+        To render a template:
+            from compiler import compiler
+            metadata = metadata_dictionary
+            template = "MDT.yaml"
+            DT = compiler.compile(template, metadata)
+    Configuration parameters:
+       templates_path: path to YAML templates folder [Default value 'None' sets the path under the compiler]
+       modules: List of modules to import in Jinja2 [Default value 'None']
+    Functions:
+        init - initialize the compiler by setting templates path, and modules
+        compile - select a template and provide metadeta to jinja to get a DT
+    Implementation:
+        Requirement - Jinja2
+        Functional programming.
+    Tests:
+        Unit test wih pytest:
+            module: tests.test_compiler.test_compiler.py
+                    Test scope: asserts if the output DT is equal with a sample DT in YAML format
+                    Test steps:
+                                1. initiate the compiler
+                                2. render a template with sample data
+                                3. assert if output is equal to a sample expected sample DT
+                    Requirement: ruamel.aml
  """
-#init logger
-import os.path
-import sys
 
+# TODO: update unit tests
+
+
+
+# Import the logger
 from utils.logger import logger
-import inspect
+logger.info('Compiler imported')
 
-#init jinja
-from jinja2 import Environment, FileSystemLoader
-_templates_path = None
-_templates_modules = None
+# Import the debugger
+from utils import debugger
 
-#init compiler (set jinja globals and logging level)
-def init(templates_path, templates_modules):
+# Compiler global
+_templates_path = None # PRIVATE - Path to templates
+_modules = [] # PRIVATE - List of modules to import in Jinja2
+
+# Function to initiate the compiler
+@debugger.debug
+def init(templates_path, modules):
+    '''
+    Compiler init function:
+    Set globals templates_path, modules based on input arguments.
+    :param templates_path: Path to templates
+    :param modules: List of modules to import in Jinja2
+    :return: None
+    '''
     global _templates_path
-    global _templates_modules
+    global _modules
     _templates_path = templates_path
-    _templates_modules = templates_modules
+    _modules = modules
     logger.info('Compiler has been initiated')
 
-
-#compile jinja template (logger decorator)
-#@logger_util.logger_func(__name__, __file__, inspect.currentframe().f_lineno)
+# Function to get a DT of specific type based on the provided metadata
+@debugger.debug
 def compile(type, metadata):
-    # logger_util.caller = inspect.stack()[2][1]
-    # logger_util.caller_line = inspect.stack()[2][2]
+    '''
+    Compiler compile function:
+    Get a DT by providing the type of the DT along with required metadata.
+    :param type: String with the name of the template [available templates 'algodt.yml', idt.yml', 'mdt.yml']
+    :param metadata: Dictionary with values to populate in the template.
+    :return: String with the DT
+    '''
+    logger.info(f'Attempting to render a(n) {type}')
+    from jinja2 import Environment, FileSystemLoader
+    #load the templates
     env = Environment(loader=FileSystemLoader(_templates_path))
-    dt = env.get_template(type)
-    for module in _templates_modules:
-        exec(module[1])
-        for function in module[2]:
-            function_call = f'{module[0]}.{function}'
-            function_to_run = eval(function_call)
-            dt.globals[function] = function_to_run
-    return dt.render(metadata)
+    # Try to load the template, if not exists return an error
+    logger.info(f'Loading template: {type}')
+    try:
+        dt = env.get_template(type)
+    except:
+        logger.error(f'TemplateNotFound: {type}')
+        return f'TemplateNotFound: {type}'
+    logger.info(f'Template loaded')
+    logger.info(f'Importing modules and functions (if any)')
+    # Each module contains: position [0] module name, [1] import statement, [2] functions
+    for module in _modules:
+        # Try to import a module, if not exists return an error
+        try:
+            # execute import statement
+            exec(module[1])
+            logger.info(f'module imported: [{module[0]}]')
+        except Exception as e:
+            logger.error(f'ModuleNotFoundError: {e}')
+            return f'ModuleNotFoundError: {e}'
+        logger.info(f'Loading module [{module[0]}] functions to jinja2 (if any)')
+        # Try to load functions for a module, if not exist return an error
+        try:
+            for function in module[2]:
+                function_call = f'{module[0]}.{function}'
+                try:
+                    # Evaluate a function and add it to the jinja2 globals
+                    function_to_run = eval(function_call)
+                    dt.globals[function] = function_to_run
+                    logger.info(f'Function loaded: [{function}]')
+                except Exception as e:
+                    print(f'Function [{function}] not exist: {e}')
+                    logger.error(f'Function [{function}] not exist: {e}')
+        except:
+            logger.warning('No functions loaded to jinja2')
+    logger.info('Trying to compile the DT')
+    try:
+        dt = dt.render(metadata)
+        logger.info('Compilation completed successfully')
+        return dt
+    except Exception as e:
+        logger.error(f'Compilation failed: {e}')
+        return f'Compilation failed: {e}'
 
 
