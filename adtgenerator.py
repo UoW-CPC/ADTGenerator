d6da6d261bda796e8c64cc36775a899a204fcbcc
Hamed Hamzeh <78861297+hmdcpc2021@users.noreply.github.com>
2021-11-02 16:18:37 +0000

    Update adtgenerator.py

--- adtgenerator.py
+++ adtgenerator.py
@@ -16,7 +16,6 @@ adtg_restapi.init()
 
 if __name__ == '__main__':
     from adtg_conf import CONFIG
-    compiler.init(CONFIG['compiler'], adtg_restapi.log)
     adtg_restapi.app.run(debug=adtg_conf.CONFIG.get('service', dict()).get('flask_debug_mode', True), 
             host=adtg_conf.service_host, 
             port=adtg_conf.service_port)
