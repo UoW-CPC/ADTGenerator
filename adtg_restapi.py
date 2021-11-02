94c5ded1d076792d57addaa86d94502d8176c0ed
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2021-11-02 16:58:28 +0000

    Add initial version of request exception handler

--- adtg_restapi.py
+++ adtg_restapi.py
@@ -10,6 +10,25 @@ app = None
 oidc = None
 oidc_enabled = False
 
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+class RequestException(Exception):
+    def __init__(self, status_code, reason, *args):
+        super(RequestException, self).__init__(*args)
+        self.status_code, self.reason = status_code, reason
+    def to_dict(self):
+        return dict(status_code=self.status_code,
+                    reason=self.reason,
+                    message=str(self))
+
+@app.errorhandler(RequestException)
+def handled_exception(error):
+    log.error('An exception occured: %r', error)
+    #print error.to_dict()
+    return jsonify(error.to_dict())
+
+
 def perform_compile(type):
     global log
     log.debug('Compile '+type+' started')
@@ -34,8 +53,6 @@ def init():
     logging.config.dictConfig(adtg_conf.CONFIG['logging'])
     log = logging.getLogger('adtg')
 
-    app = Flask(__name__)
-    app.config.from_object(__name__)
     
     oidc_enabled = adtg_conf.CONFIG.get('service', dict()).get('enable_oidc', False)
     oidc_require_token = adtg_conf.CONFIG.get('service', dict()).get('check_user_token', False)
