c2d58c9561f38ee22022c2d5bad626dbec88d0ed
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2021-11-02 16:48:45 +0000

    rollback unnecessary changes

--- adtg_restapi.py
+++ adtg_restapi.py
@@ -2,64 +2,35 @@ from flask import Flask, jsonify, request
 import logging,logging.config, json
 from flask_oidc import OpenIDConnect
 import adtg_conf
-from werkzeug.exceptions import BadRequest, InternalServerError
-from functools import wraps
-from typing import Any as EndpointResult
+
 from compiler import compiler
 
 log = None
 app = None
 oidc = None
 oidc_enabled = False
-app = Flask(__name__)
 
-def validate_json(f):
-    @wraps(f)
-    def wrapper(*args, **kw):                  
-        try:
-            request.get_json()
-        except BadRequest as e:
-            msg = "POST request must be a valid json"
-            log.error(msg)
-            return jsonify({"Bad Request": msg, "error code": 400})
-        return f(*args, **kw)
-    return wrapper
-
-@app.errorhandler(404)
-def page_not_found(error):
-   log.error('Page not found'), 404
-   return jsonify({"error_code": 404, "message":"Page not found; something went wrong!"})
-
-@app.errorhandler(InternalServerError)
-def handle_unexpected_error(e: Exception) -> EndpointResult:
-    log.exception('Unknown error', exc_info=e)
-    return jsonify({
-        'error_code': '500',
-        'error_type': 'Internal Server Error',
-    }), InternalServerError.code
-
-@validate_json
 def perform_compile(type):
     global log
     log.debug('Compile '+type+' started')
     if oidc_enabled:
-        token = oidc.get_access_token()
-    input_data = request.get_json()
+        token = oidc.get_access_token()    
+    input_data = request.json
+    if not input_data:
+        raise RequestException(400, 'No valid JSON input found')
+     
     log.debug('This is a JSON request: {0}'.format(input_data))
+    print(input_data)
    
-    try:
-        template_file = adtg_conf.CONFIG.get('compiler',dict()).get('templates',dict()).get(type)
-        result = compiler.compile(template_file, input_data, log)
-        log.debug('Compile '+type+' finished')
-        return json.loads(json.dumps(result, sort_keys=True, indent=4, separators=(',', ': ')))
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    template_file = adtg_conf.CONFIG.get('compiler',dict()).get('templates',dict()).get(type)
+    result = compiler.compile(template_file, input_data, log)
+    log.debug('Compile '+type+' finished')
+    return jsonify(result), 200   
 
 
 def init():
     global log, app, oidc, oidc_enabled, perform_compile
 
-    adtg_conf.init()
     logging.config.dictConfig(adtg_conf.CONFIG['logging'])
     log = logging.getLogger('adtg')
 
@@ -80,4 +51,3 @@ def init():
     app.add_url_rule(endpoint, methods=['POST'], view_func=perform_compile)
 
     return
-
--- adtgenerator.py
+++ adtgenerator.py
@@ -1,21 +1,28 @@
-import logging,logging.config, adtg_restapi
+import logging,logging.config
+
 import adtg_conf
-from compiler import compiler
 import adtg_restapi
 
+from compiler import compiler
 
-adtg_conf.init()
-logging.config.dictConfig(adtg_conf.CONFIG['logging'])
-log = logging.getLogger('adtg')
-log.debug("CONFIG = "+str(adtg_conf.CONFIG))
-compiler_log = logging.getLogger('adtg_compiler')
-compiler.init(adtg_conf.CONFIG.get('compiler',dict()).get('template_directory',None),
-adtg_conf.CONFIG.get('compiler',dict()).get('modules',None), log)
-adtg_restapi.init()
-
+log = None
 
 if __name__ == '__main__':
     from adtg_conf import CONFIG
+
+    adtg_conf.init()
+    
+    logging.config.dictConfig(adtg_conf.CONFIG['logging'])
+    log = logging.getLogger('adtg')
+    log.debug("CONFIG = "+str(adtg_conf.CONFIG))
+    
+    compiler_log = logging.getLogger('adtg_compiler')
+    compiler.init(adtg_conf.CONFIG.get('compiler',dict()).get('template_directory',None), 
+                  adtg_conf.CONFIG.get('compiler',dict()).get('modules',None),
+                  log)
+    
+    adtg_restapi.init()
+
     adtg_restapi.app.run(debug=adtg_conf.CONFIG.get('service', dict()).get('flask_debug_mode', True), 
             host=adtg_conf.service_host, 
             port=adtg_conf.service_port)
