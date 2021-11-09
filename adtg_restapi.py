c7ea17b7d14dd5907ad17dd42ca396a73d1b728b
Hamed Hamzeh <78861297+hmdcpc2021@users.noreply.github.com>
2021-11-09 09:51:07 +0000

    Update adtg_restapi.py

--- adtg_restapi.py
+++ adtg_restapi.py
@@ -48,8 +48,6 @@ def perform_compile(type):
     log.debug('Compile '+type+' started')
     if oidc_enabled:
         token = oidc.get_access_token()    
-    if type not in adtg_conf.CONFIG.get('compiler',dict()).get('templates',dict()).keys():
-        raise RequestException(400, 'No valid type for compilation defined!')
     input_data = request.get_json()
     log.debug('This is a JSON request: {0}'.format(input_data))
    
