cfd9c2469b498051fd11b2297e7cf11dceae0e5d
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2021-11-26 16:31:21 +0000

    Add sample metadata files and idt->ddt

--- examples/compile-ddt.sh
+++ examples/compile-ddt.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ] ; then
   exit 1
 fi
 
-curl http://127.0.0.1:4000/v1/adtg/compile/idt -X POST -H "Content-Type: application/json" -d @"$1" 
+curl http://127.0.0.1:4000/v1/adtg/compile/ddt -X POST -H "Content-Type: application/json" -d @"$1" 
