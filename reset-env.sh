1b0838b2ab66689e62adb418891faa8f8914e593
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2022-01-07 09:41:00 +0000

    add installation of micado-parser

--- reset-env.sh
+++ reset-env.sh
@@ -9,6 +9,7 @@ rm -rf "$PDIR"
 virtualenv -p python3 "$PDIR"
 source "$PDIR"/bin/activate
 pip install -r requirements.txt 
+pip install git+git://github.com/micado-scale/micado-parser@main#egg=micado-parser
 
 set +ex
 echo "Now, you can execute 'run.sh' whenever you need the ADTG service"
