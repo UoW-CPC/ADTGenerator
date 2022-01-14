7f45453ef220d61700d723f45441cf7235005f21
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2022-01-14 14:23:06 +0000

    Adding kompose installation to reset-env.sh

--- reset-env.sh
+++ reset-env.sh
@@ -10,6 +10,9 @@ virtualenv -p python3 "$PDIR"
 source "$PDIR"/bin/activate
 pip install -r requirements.txt 
 
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.1/kompose-linux-amd64 -o $PDIR/bin/kompose
+chmod +x $PDIR/bin/kompose
+
 set +ex
 echo "Do not forget to activate your virtual environment: source $PDIR/bin/activate"
 echo "If environment is activated, 'run.sh' to launch the ADT Generator service"
