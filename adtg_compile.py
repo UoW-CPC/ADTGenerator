2d025b435fa9cf9ae9201c3f151e4dbb7cf4ec86
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2022-10-13 12:18:17 +0000

    add rendering open parameters

--- adtg_compile.py
+++ adtg_compile.py
@@ -1,11 +1,11 @@
 from adtg_file import *
 from dockubeadt.translator import translate_dict
 import jinja2
-import yaml
+import ruamel.yaml
 from pathlib import Path
 import json
 import contextlib
-import sys 
+import sys, io 
 from io import StringIO
 
 def raise_helper(msg):
@@ -20,6 +20,18 @@ def save_compile_stdout(log, full_wd, stdout, stderr):
         for l in lines:
             add_log(full_wd, "    "+l+"\n")
 
+def rendering_open_parameters(data):
+    if isinstance(data, dict):
+        return {k: rendering_open_parameters(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [rendering_open_parameters(i) for i in data]
+    else:
+        d = str(data)
+        if d.startswith("open_parameter(") and d.endswith(")"):
+            parameter = d[15:len(d)-1].strip().strip('\"').strip('\'')
+            d={'get_input':parameter}
+        return d
+
 def compile(log, full_wd, asset_type, asset_dict, template_file):
     if asset_type == "mdt":
         with contextlib.redirect_stdout(StringIO()) as o, contextlib.redirect_stderr(StringIO()) as e:
@@ -33,7 +45,18 @@ def compile(log, full_wd, asset_type, asset_dict, template_file):
                 save_compile_stdout(log, full_wd, o.getvalue(), e.getvalue())
                 raise
         
-        asset_dict['adt_from_dockubeadt']=dockubeadt_result
+        yaml = ruamel.yaml.YAML()
+        #workaround due to the fact that dockubeadt returns node_templates as yaml instead of dict
+        dockubeadt_result = yaml.load(str(dockubeadt_result))
+        #workaround finishes here
+        ms_dict = rendering_open_parameters(dockubeadt_result)
+        buf = io.BytesIO()
+        yaml.dump(ms_dict,buf)
+        ms_yaml = str(buf.getvalue().decode("utf-8"))
+        ms_yaml_with_2spaces = ""
+        for line in ms_yaml.split('\n'):
+            ms_yaml_with_2spaces +="  "+line+"\n"
+        asset_dict['adt_from_dockubeadt']=ms_yaml_with_2spaces
 
     template = Path(template_file).read_text()
     asset_dict['raise'] = raise_helper
