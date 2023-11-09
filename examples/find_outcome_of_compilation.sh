b1de88b4c13e5d4a73ed8163efd4f1400dbc2b5c
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2023-11-09 12:35:10 +0000

    feat: separating id and name in query script

--- examples/find_outcome_of_compilation.sh
+++ examples/find_outcome_of_compilation.sh
@@ -3,19 +3,19 @@
 for dir in `ls -d */`; 
 	do 
 		cd $dir;
-	        #echo $dir
 
 		START=`grep -e "Generating ADT for Process" generate.log | grep -e "starts..."`
 		FINISH=`grep -e "Generating ADT for Process" generate.log | grep -e "finished."`;
-		#echo "START: $START"
-		#echo "FINISH: $FINISH"
+
+		DMA=`echo $START | sed -e 's/Generating ADT for Process //' | sed -e 's/ starts...//'`;
+		#id=`echo $DMA | cut -f3 -d"\""`
+		id=""
+		name=`echo $DMA | cut -f2 -d"\""`
 
 		if [[ ! -z "$FINISH" ]]; then
-			DMA=`echo $FINISH | sed -e 's/Generating ADT for Process //' | sed -e 's/ finished.//'`; 
-			echo "SUCCESS: $dir $DMA";
+			echo "SUCCESS: $dir$id $name";
 		else	
-			DMA=`echo $START | sed -e 's/Generating ADT for Process //' | sed -e 's/ starts...//'`;
-			echo "FAILURE: $dir $DMA";
+			echo "FAILURE: $dir$id $name";
 		fi
 		cd ..;
 	done
