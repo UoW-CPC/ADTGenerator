b6191e9e370baff0b056bf04b5dc88fd74197861
Jozsef Kovacs <J.Kovacs@westminster.ac.uk>
2024-01-12 13:28:09 +0000

    feat: add start() API method for background ADT generation

--- adtg_restapi.py
+++ adtg_restapi.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import BadRequest, InternalServerError
 import adtg_conf
 from functools import wraps
 from typing import Any as EndpointResult
+import threading
 
 import adtg_generate
 import adtg_compile
@@ -109,6 +110,44 @@ def generate():
         log.info("Response JSON: "+str(response))
         return response, 400
 
+@validate_json
+def start():
+    global log
+    log.info('ADT generation starts in the background...')
+    input_data = request.get_json()
+    try:
+        root_wd = adtg_conf.CONFIG.get('generator',dict())['working_directory']
+        id = adtg_generate.init_working_directory(root_wd)
+        log.info('  ID: '+id)
+    except Exception as e:
+        log.info('Starting the ADT generation process FAILED')
+        log.exception(e)
+        response = jsonify( {"success": False,
+                             "message": "Starting the ADT generation process FAILED\n"+
+                                        str(e)} )
+        log.info("Response JSON: "+str(response))
+        return response, 500
+    try:
+        th = threading.Thread(  target=adtg_generate.launch_generate,
+                                args=(log, root_wd, id, input_data) )
+        th.daemon = True
+        th.start()
+        log.info('Starting the ADT generation process DONE')
+        response = jsonify( {"success": True,
+                             "id" : id,
+                             "message": "Starting the ADT generation process DONE"} )
+        log.info("Response JSON: "+str(response))
+        return response, 200
+    except Exception as e:
+        log.info('Starting the ADT generation process FAILED.')
+        log.exception(e)
+        response = jsonify( {"success": False,
+                             "message": "Starting the ADT generation process FAILED\n"+
+                                        str(e)} )
+        log.info("Response JSON: "+str(response))
+        return response, 400
+
+
 def status(id):
     log.debug("status() invoked: "+id)
     root_wd = adtg_conf.CONFIG.get('generator',dict()).get('working_directory')
@@ -116,8 +155,9 @@ def status(id):
     if os.path.isdir(full_wd):
         response_file_path = os.path.join(full_wd, adtg_utils.response_file_name)
         if os.path.isfile(response_file_path):
-            response = jsonify(adtg_utils.read_file(response_file_path))
-            return response, 200
+            response = adtg_utils.read_file(response_file_path)
+            response_json = jsonify(response)
+            return response, 200 if response["success"] else 400
     return jsonify({"message": "Cannot find ADT generation with this ID!"}), 400
 
 def download(dir,file):
@@ -130,7 +170,7 @@ def health():
     return '', 200
 
 def init():
-    global log, app, oidc, oidc_enabled, compile, generate, status, download
+    global log, app, oidc, oidc_enabled, compile, generate, start, status, download
 
     logging.config.dictConfig(adtg_conf.CONFIG['logging'])
     log = logging.getLogger('adtg')
@@ -144,6 +184,7 @@ def init():
         oidc = OpenIDConnect(app)
         compile = (oidc.accept_token(require_token=oidc_require_token))(compile)
         generate = (oidc.accept_token(require_token=oidc_require_token))(generate)
+        start= (oidc.accept_token(require_token=oidc_require_token))(start)
         status = (oidc.accept_token(require_token=oidc_require_token))(status)
         download = (oidc.accept_token(require_token=oidc_require_token))(download)
         
@@ -154,6 +195,10 @@ def init():
     endpoint=adtg_conf.rest_root_path+'/generate'
     log.debug("Registering generate() method for endpoint "+endpoint)
     app.add_url_rule(endpoint, methods=['POST'], view_func=generate)
+    
+    endpoint=adtg_conf.rest_root_path+'/start'
+    log.debug("Registering start() method for endpoint "+endpoint)
+    app.add_url_rule(endpoint, methods=['POST'], view_func=start)
     
     endpoint=adtg_conf.rest_root_path+'/status/<id>'
     log.debug("Registering status() method for endpoint "+endpoint)
---
+++ examples/start-adt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+  echo "Argument is missing. Please, specify a DMA json file as argument!"
+  exit 1
+fi
+#-w "%{http_code}"
+
+curl -w "HTTP code: %{http_code}\n" http://127.0.0.1:4000/v1/adtg/start -X POST -H "Content-Type: application/json" -d @"$1" 
